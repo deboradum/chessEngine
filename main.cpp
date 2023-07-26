d5f755f3340cfee30579202e116afa57e44a49ee
deboradum <pvwpvw@protonmail.com>
2023-07-26 10:48:18 +0200

    Added fen parsing

--- main.cpp
+++ main.cpp
@@ -1,42 +1,138 @@
+#include <_ctype.h>
+#include <cctype>
 #include <iostream>
 #include <string>
+#include <bitset>
+#include <cstddef>
 using namespace std;
 
-static class Piece {
-    public:
-        const int None = 0;
-        const int King = 1;
-        const int Queen = 2;
-        const int Rook = 3;
-        const int Bishop = 4;
-        const int Knight = 5;
+vector<string> convertFen(string fen) {
+    vector<string> result;
+    string line = "";
 
-        const int White = 8;
-        const int Black = 16;
+    for (char& c : fen) {
+        // Inserts current line to vector on delimiter.
+        if (c == '/') {
+            result.push_back(line);
+            line = "";
+            continue;
+        }
+        // Appends to current line.
+        line.push_back(c);
+    }
+    // Inserts last line to vector.
+    result.push_back(line);
+
+    return result;
+}
+
+class Piece {
+    public:
+        const bitset<5> None{0};
+        const bitset<5> King{1};
+        const bitset<5> Queen{2};
+        const bitset<5> Rook{3};
+        const bitset<5> Bishop{4};
+        const bitset<5> Knight{5};
+        const bitset<5> Pawn{6};
+
+        const bitset<5> White{8};
+        const bitset<5> Black{16};
 };
 
 class Board {
     public:
-        int square[64] = {};
+        vector< vector<bitset<5>> > square = {{bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}},
+                                              {bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}},
+                                              {bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}},
+                                              {bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}},
+                                              {bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}},
+                                              {bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}},
+                                              {bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}},
+                                              {bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}, bitset<5>{0}}};
         vector<string> movesMade = {};
 
         // Constructor.
-        Board(string startPos, vector<string> moves) {
-            fenToBoard(startPos);
+        Board(string startPosFen, vector<string> moves) {
+            setupBoard(convertFen(startPosFen));
 
             for (string move : moves) {
                 makeMove(move);
                 movesMade.push_back(move);
             }
+        }
 
-            cout << square[0] << square[1] << square[2] << square[63] << endl;
+        void printBoard() {
+            for (vector<bitset<5>> line : square) {
+                for (bitset<5> piece : line) {
+                    cout << piece << " ";
+                }
+                cout << "\n";
+            }
         }
     
     private:
-        void fenToBoard(string fen) {
-            
+        void setupBoard(vector<string> fen) {
+            Piece Piece;
+            int rank = 0;
+            for (string line : fen) {
+                int file = 0;
+                for (char& c : line) {
+                    if (isdigit(c)) {
+                        file += int(c)-48;
+                        continue;
+                    }
+                    switch (tolower(c)) {
+                        case 'k':
+                            if (isupper(c)) {
+                                square[rank][file] = Piece.White | Piece.King;
+                            } else {
+                                square[rank][file] = Piece.Black | Piece.King;
+                            }
+                            break;
+                        case 'q':
+                            if (isupper(c)) {
+                                square[rank][file] = Piece.White | Piece.Queen;
+                            } else {
+                                square[rank][file] = Piece.Black | Piece.Queen;
+                            }
+                            break;
+                        case 'r':
+                            if (isupper(c)) {
+                                square[rank][file] = Piece.White | Piece.Rook;
+                            } else {
+                                square[rank][file] = Piece.Black | Piece.Rook;
+                            }
+                            break;
+                        case 'b':
+                            if (isupper(c)) {
+                                square[rank][file] = Piece.White | Piece.Bishop;
+                            } else {
+                                square[rank][file] = Piece.Black | Piece.Bishop;
+                            }
+                            break;
+                        case 'n':
+                            if (isupper(c)) {
+                                square[rank][file] = Piece.White | Piece.Knight;
+                            } else {
+                                square[rank][file] = Piece.Black | Piece.Knight;
+                            }
+                            break;
+                        case 'p':
+                            if (isupper(c)) {
+                                square[rank][file] = Piece.White | Piece.Pawn;
+                            } else {
+                                square[rank][file] = Piece.Black | Piece.Pawn;
+                            }
+                            break;
+                    }
+                    file++;
+                }
+                rank++;
+            }
         }
 
+        // Makes a move on the board.
         void makeMove(string move) {
 
         }
